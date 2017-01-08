3445f379a8c67ce711b7443c41bd63337a05a311
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-01-08 22:54:42 +0200

    Calculate bmi to the ui

--- WhatIsMyBMI/ViewController.swift
+++ WhatIsMyBMI/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var heightValue: UILabel!
     @IBOutlet weak var weightSlider: UISlider!
     @IBOutlet weak var weightValue: UILabel!
+    var heightValueState: Int?
+    var weightValueState: Int?
+    @IBOutlet weak var bmiResult: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +31,28 @@ class ViewController: UIViewController {
     private func roundToString(sliderValue : Float) -> String {
         return String(Int(sliderValue))
     }
+    
+    private func calculateBMI(weight : Int, height : Int) -> Double {
+        let heightInMeters = Double(height) / Double(100)
+        let heightSquared = pow(heightInMeters, 2.0)
+        let unRoundedBMI = Double(weight) / heightSquared
+        return Double(round(10 * unRoundedBMI) / 10)
+    }
 
     @IBAction func onHeightChange(_ sender: Any) {
+        heightValueState = Int(heightSlider.value)
         heightValue.text = "\(roundToString(sliderValue: heightSlider.value)) cm"
     }
 
     @IBAction func onWeightChange(_ sender: Any) {
+        weightValueState = Int(weightSlider.value)
         weightValue.text = "\(roundToString(sliderValue: weightSlider.value)) kg"
+    }
+    @IBAction func onCalculateButtonTap(_ sender: Any) {
+        if heightValueState != nil && weightValueState != nil {
+            let bmi = calculateBMI(weight: weightValueState!, height: heightValueState!)
+            bmiResult.text = String(bmi)
+        }
     }
 }
 
